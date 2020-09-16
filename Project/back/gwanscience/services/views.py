44c85a574f63dccad2f07631f03bde596ae3674f
Young Hwan Kim <aorkslrdnjwm@naver.com>
2020-09-16 10:44:32 +0900

    [+] create app, url path setting

---
+++ Project/back/gwanscience/services/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    pass
+
+def face_reading(request):
+    pass
+
+def face_reading_reading(request):
+    pass
+
+def name_compability(request):
+    pass
+
+def name_compability_compability(request):
+    pass
