0978579d81aa6353338434fd68508704373cc09b
Young Hwan Kim <aorkslrdnjwm@naver.com>
2020-09-23 11:56:34 +0900

    [+] update face_reading

--- Project/front/gwanscience/src/views/result.vue
+++ Project/front/gwanscience/src/views/result.vue
@@ -1,26 +1,38 @@
 <template>
   <div>
-    <h1>
-      관상 분석 결과
-    </h1>
+    <h1>{{userInfo.nickname}}님의 관상 분석 결과</h1>
     <div>
-  <b-tabs content-class="mt-3">
-    <b-tab title="눈" active><p></p></b-tab>
-    <b-tab title="눈썹" active><p></p></b-tab>
-    <b-tab title="코"><p></p></b-tab>
-    <b-tab title="입"><p></p></b-tab>
-    <b-tab title="종합 평가"><p></p></b-tab>
-  </b-tabs>
-</div>
+      <b-tabs content-class="mt-3">
+        <b-tab title="눈">
+          <p></p>
+        </b-tab>
+        <b-tab title="눈썹">
+          <p></p>
+        </b-tab>
+        <b-tab title="코">
+          <p></p>
+        </b-tab>
+        <b-tab title="입">
+          <p></p>
+        </b-tab>
+        <b-tab title="종합 평가" active>
+          <p></p>
+        </b-tab>
+      </b-tabs>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name: "result"
-}
+  name: "result",
+  props: {
+    userInfo: {
+      type: Object,
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
