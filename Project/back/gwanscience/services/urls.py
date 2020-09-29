57fecc3bec38431b56e722bc1c1ef5d688f2b819
김익한 <kt0831@naver.com>
2020-09-29 15:10:00 +0900

    [+] add name_compatbility front

--- Project/back/gwanscience/services/urls.py
+++ Project/back/gwanscience/services/urls.py
@@ -5,8 +5,9 @@ app_name = 'services'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/face_reading/', views.face_reading, name='face_reading'),
-    path('/face_reading/reading/', views.face_reading_reading, name='face_reading_reading'),
-    path('/name_compability/', views.name_compability, name='name_compability'),
-    path('/name_compability/compability/', views.name_compability_compability, name='name_compability_compability'),
+    path('face_reading/', views.face_reading, name='face_reading'),
+    path('face_reading/reading/', views.face_reading_reading, name='face_reading_reading'),
+    # path('name_compability/<str:name_1>/<str:name_2>/', views.name_compability, name='name_compability'),
+    path('name_compability/', views.name_compability, name='name_compability'),
+    path('name_compability/compability/', views.name_compability_compability, name='name_compability_compability'), # 아마 안쓸 듯?
 ]
