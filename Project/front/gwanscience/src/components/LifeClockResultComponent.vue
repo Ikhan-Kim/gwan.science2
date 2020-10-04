688de2f638892f9e92c30fcbe8d4ada80c9fb4ff
Young Hwan Kim <aorkslrdnjwm@naver.com>
2020-10-04 22:44:15 +0900

    [+] update 인생시계 완성

--- Project/front/gwanscience/src/components/LifeClockResultComponent.vue
+++ Project/front/gwanscience/src/components/LifeClockResultComponent.vue
@@ -2,8 +2,7 @@
   <div>
     <h1>Life Clock</h1>
     <h3>당신의 인생 시간은 {{ this.result.time }} 입니다.</h3>
-    <img src="this.img_url" />{{ this.img_url }}
-    <button @click="test()">test</button>
+    <img :src="getImgUrl(this.result.img_url)" />
     <p>{{ this.result.ment }}</p>
   </div>
 </template>
@@ -37,14 +36,6 @@ export default {
     loadImg() {
       this.img_url = "../assets/" + this.result.img_url;
       return "../assets/" + this.result.img_url;
-    },
-    test() {
-      console.log(
-        this.result.time,
-        this.result.ment,
-        this.result.img_url,
-        this.img_url
-      );
     },
   },
 };
