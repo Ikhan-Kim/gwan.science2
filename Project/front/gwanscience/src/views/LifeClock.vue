db898f62884c6d4dc8e916e4c62235f857a6f0f3
Young Hwan Kim <aorkslrdnjwm@naver.com>
2020-10-04 16:34:08 +0900

    [+] update LifeClock.vue

--- Project/front/gwanscience/src/views/LifeClock.vue
+++ Project/front/gwanscience/src/views/LifeClock.vue
@@ -1,7 +1,35 @@
-<template></template>
+<template>
+  <div>
+    <h1>Life Clock</h1>
+    <div class="row d-flex justify-content-center m-md-2">
+      <b-form-input
+        style="width: 300px"
+        type="number"
+        v-model="userInfo.age"
+        placeholder="나이를 입력해주세요."
+      ></b-form-input>
+      <router-link
+        :to="{ name: 'LifeClockResult', params: { userInfo: userInfo } }"
+      >
+        <b-button v-if="isPhotoTaken == true" style="margin-left: 20px"
+          >나의 인생 시간은? →</b-button
+        >
+      </router-link>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "LifeClock",
+  data() {
+    return {
+      userInfo: {
+        age: 0,
+      },
+    };
+  },
+};
 </script>
 
 <style>
