988b2f2eed26869cab61c5541cc561afd3c6c15b
Young Hwan Kim <aorkslrdnjwm@naver.com>
2020-10-07 00:07:40 +0900

    [+] 핫픽스

--- Project/front/gwanscience/src/views/FaceReading.vue
+++ Project/front/gwanscience/src/views/FaceReading.vue
@@ -1,45 +1,49 @@
 <template>
   <div>
-    <div class="f-ujr duru"><h3 class="pt-20"> 제 ＜1＞ 법칙. 관상분석</h3></div>
-    <div class="f-ys" v-if="!isCameraOpen"><h5>기본정보를 입력해주세요.</h5><br></div>
-    <div class="f-ys" v-else><h5>정확한 관상 분석을 위해 <br>중앙에 얼굴이 오도록 촬영해주세요.
-</h5></div>
-    
-    <br>
-    <br>
+    <div class="f-ujr duru"><h3 class="pt-20">제 ＜1＞ 법칙. 관상분석</h3></div>
+    <div class="f-ys" v-if="!isCameraOpen">
+      <h5>기본정보를 입력해주세요.</h5>
+      <br />
+    </div>
+    <div class="f-ys" v-else>
+      <h5>정확한 관상 분석을 위해 <br />중앙에 얼굴이 오도록 촬영해주세요.</h5>
+    </div>
+
+    <br />
+    <br />
 
     <b-container class="bv-example-row f-ujr" v-if="!isCameraOpen">
       <b-row>
-        <b-col cols="4" class="pb-3"><h4> 닉네임</h4></b-col>
+        <b-col cols="4" class="pb-3"><h4>닉네임</h4></b-col>
         <b-col cols="8">
           <b-form-input
-          type="text"
-          v-model="userInfo.nickname"
-          placeholder="닉네임을 입력해주세요."
-        ></b-form-input>
+            type="text"
+            v-model="userInfo.nickname"
+            placeholder="닉네임을 입력해주세요."
+          ></b-form-input>
         </b-col>
         <div class="w-100"></div>
-        <b-col cols="4" class="pb-3"> <h4> 나이</h4></b-col>
+        <b-col cols="4" class="pb-3"> <h4>나이</h4></b-col>
         <b-col cols="8">
           <b-form-input
-          type="number"
-          v-model="userInfo.age"
-          placeholder="나이를 입력해주세요."
-        ></b-form-input>
+            type="number"
+            v-model="userInfo.age"
+            placeholder="나이를 입력해주세요."
+          ></b-form-input>
         </b-col>
         <div class="w-100"></div>
-        <b-col cols="4" class="pb-3"> <h4> 성별</h4></b-col>
+        <b-col cols="4" class="pb-3"> <h4>성별</h4></b-col>
         <b-col cols="8">
           <b-form-radio-group
-          v-model="userInfo.gender"
-          :options="options"
-        ></b-form-radio-group>
+            v-model="userInfo.gender"
+            :options="options"
+          ></b-form-radio-group>
         </b-col>
-     </b-row>
+      </b-row>
     </b-container>
 
     <!-- 사진 촬영 버튼 -->
-    
+
     <!-- 📌📌📌 작동 이상 없으면 삭제하기 !! -->
     <!-- <div class="container" v-if="!isCameraOpen">
       <div class="row d-flex justify-content-center m-md-2">
@@ -70,17 +74,18 @@
     <!-- 사진촬영 버튼 -->
     <div class="container">
       <div class="row d-flex justify-content-center">
-        
-          <button v-if="!isCameraOpen" 
-            class="btn-customm bg-red f-ujr" style="width: 60%"
-            :class="{
-              'bg-red': !isCameraOpen,
-              'bg-green': isCameraOpen,
-            }"
-            @click="checkInfo"
-          >
-            <span class="bg-red h4">사진 촬영</span>
-          </button>
+        <button
+          v-if="!isCameraOpen"
+          class="btn-customm bg-red f-ujr"
+          style="width: 60%"
+          :class="{
+            'bg-red': !isCameraOpen,
+            'bg-green': isCameraOpen,
+          }"
+          @click="checkInfo"
+        >
+          <span class="bg-red h4">사진 촬영</span>
+        </button>
         <div class="camera-button"></div>
       </div>
       <div class="row d-flex justify-content-center m-md-2">
@@ -103,14 +108,33 @@
         </div>
       </div>
 
-        <div class="camera-shoot mt-2 mb-5" v-if="isCameraOpen">
-          <button v-if="isPhotoTaken == false" class="btn-customm bg-red f-ujr h4" style="width: 60%" @click="takePhoto">사진촬영</button>
-          
-          <button v-if="isPhotoTaken == true" class="btn-customm bg-green f-ujr mr-4 h5" style="width: 30%" @click="takePhoto">다시찍기</button>
-          <router-link :to="{ name: 'FaceReadingResult', params: { userInfo: userInfo } }">
-          <button v-if="isPhotoTaken == true" class="btn-customm f-ujr bg-red h5" style="width: 30%">관상보기</button>
-          </router-link>
-        </div>
+      <div class="camera-shoot mt-2 mb-5" v-if="isCameraOpen">
+        <button
+          v-if="isPhotoTaken == false"
+          class="btn-customm bg-red f-ujr h4"
+          style="width: 60%"
+          @click="takePhoto"
+        >
+          사진촬영
+        </button>
+
+        <button
+          v-if="isPhotoTaken == true"
+          class="btn-customm bg-green f-ujr mr-4 h5"
+          style="width: 30%"
+          @click="takePhoto"
+        >
+          다시찍기
+        </button>
+        <button
+          v-if="isPhotoTaken == true"
+          class="btn-customm f-ujr bg-red h5"
+          style="width: 30%"
+          @click="imgToBack()"
+        >
+          관상보기
+        </button>
+      </div>
 
       <!-- <div class="row d-flex justify-content-center m-md-2">
         <div class="camera-shoot"  v-if="isPhotoTaken">
@@ -166,15 +190,12 @@ export default {
     checkInfo() {
       if (this.userInfo.nickname == "") {
         alert("닉네임을 입력해 주세요.");
-      }
-      else if (this.userInfo.age == null) {
-        alert("나이를 입력해 주세요.")
-      }
-      else if (this.userInfo.gender == null) {
-        alert("성별을 선택해 주세요.")
-      }
-      else {
-        this.toggleCamera()
+      } else if (this.userInfo.age == null) {
+        alert("나이를 입력해 주세요.");
+      } else if (this.userInfo.gender == null) {
+        alert("성별을 선택해 주세요.");
+      } else {
+        this.toggleCamera();
       }
     },
     checkAge() {
@@ -223,7 +244,9 @@ export default {
 
       // console.log(context.canvas.toDataURL());
       this.userInfo.userPhoto = context.canvas.toDataURL();
-      this.tmpphoto = document.getElementById("photoTaken").toDataURL("image/jpeg");
+      this.tmpphoto = document
+        .getElementById("photoTaken")
+        .toDataURL("image/jpeg");
       // this.tmpphoto = document.getElementById("photoTaken").toDataURL()
       // console.log(this.tmpphoto)
 
@@ -279,14 +302,15 @@ export default {
       });
     },
     sendImage() {
-      axios.post(`http://127.0.0.1:8000/services/test`, this.tmpphoto)
-      .then(res=> {
-        console.log(res)
-        console.log('보내짐')
-      })
-      .catch(err=> {
-        console.log(err)
-      })
+      axios
+        .post(`http://127.0.0.1:8000/services/test`, this.tmpphoto)
+        .then((res) => {
+          console.log(res);
+          console.log("보내짐");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
 };
@@ -303,21 +327,22 @@ export default {
 }
 
 .pt-20 {
-  padding-top:  20px;
+  padding-top: 20px;
 }
 
 .btn-customm {
-    display: inline-block;
-    font-weight: 400;
-    text-align: center;
-    vertical-align: middle;
-    background-color: (192, 0, 0);
-    user-select: none;
-    border: 1px solid transparent;
-    padding: 0.375rem 0.75rem;
-    font-size: 1rem;
-    line-height: 1.5;
-    border-radius: 0.25rem;
-    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  display: inline-block;
+  font-weight: 400;
+  text-align: center;
+  vertical-align: middle;
+  background-color: (192, 0, 0);
+  user-select: none;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 }
 </style>
