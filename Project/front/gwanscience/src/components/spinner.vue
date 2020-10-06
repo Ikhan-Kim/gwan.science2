ffae2640075e462038aaaf657d53757917e25d06
김익한 <kt0831@naver.com>
2020-10-07 01:55:06 +0900

    [+] update name

--- Project/front/gwanscience/src/components/spinner.vue
+++ Project/front/gwanscience/src/components/spinner.vue
@@ -1,8 +1,9 @@
 <template>
   <div v-if="!loading" style="height: 500px">
-    <div class="d-flex justify-content-center mb-3">
+    <div class="d-flex justify-content-center mb-3" style="margin-top: 200px">
       <b-spinner label="Loading..."></b-spinner>
     </div>
+    <strong>{{ loadingMent }}</strong>
     <!-- <div class="d-flex align-items-center">
       <strong>Loading...</strong>
       <b-spinner class="ml-auto"></b-spinner>
@@ -15,6 +16,10 @@ export default {
   props: {
     loading: {
       type: Boolean,
+      required: true,
+    },
+    loadingMent: {
+      type: String,
       required: true,
     }
   }
