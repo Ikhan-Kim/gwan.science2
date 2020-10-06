be477b39072cf7dc3c33d08c62018a7a23a23c89
anoc00 <anoc00@naver.com>
2020-10-07 01:47:05 +0900

    [+] update FaceReading

--- Project/front/gwanscience/src/views/FaceReading.vue
+++ Project/front/gwanscience/src/views/FaceReading.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <div class="f-ujr duru"><h3 class="pt-20"> 제 ＜1＞ 법칙. 관상분석</h3></div>
-    <div class="f-ys" v-if="!isCameraOpen"><h5>기본정보를 입력해주세요.</h5><br></div>
-    <div class="f-ys" v-else><h5>정확한 관상 분석을 위해 <br>중앙에 얼굴이 오도록 촬영해주세요.
-</h5></div>
-    
+    <div class="f-ujr duru">
+      <h3 class="pt-20"> 제 ＜1＞ 법칙. 관상분석</h3>
+    </div>
+    <div class="f-ys" v-if="!isCameraOpen">
+      <h5>기본정보를 입력해주세요.</h5><br>
+    </div>
+    <div class="f-ys" v-else>
+      <h5>정확한 관상 분석을 위해 <br>중앙에 얼굴이 오도록 촬영해주세요.
+      </h5>
+    </div>
+
     <br>
     <br>
 
@@ -13,33 +19,33 @@
         <b-col cols="4" class="pb-3"><h4> 닉네임</h4></b-col>
         <b-col cols="8">
           <b-form-input
-          type="text"
-          v-model="userInfo.nickname"
-          placeholder="닉네임을 입력해주세요."
-        ></b-form-input>
+              type="text"
+              v-model="userInfo.nickname"
+              placeholder="닉네임을 입력해주세요."
+          ></b-form-input>
         </b-col>
         <div class="w-100"></div>
-        <b-col cols="4" class="pb-3"> <h4> 나이</h4></b-col>
+        <b-col cols="4" class="pb-3"><h4> 나이</h4></b-col>
         <b-col cols="8">
           <b-form-input
-          type="number"
-          v-model="userInfo.age"
-          placeholder="나이를 입력해주세요."
-        ></b-form-input>
+              type="number"
+              v-model="userInfo.age"
+              placeholder="나이를 입력해주세요."
+          ></b-form-input>
         </b-col>
         <div class="w-100"></div>
-        <b-col cols="4" class="pb-3"> <h4> 성별</h4></b-col>
+        <b-col cols="4" class="pb-3"><h4> 성별</h4></b-col>
         <b-col cols="8">
           <b-form-radio-group
-          v-model="userInfo.gender"
-          :options="options"
-        ></b-form-radio-group>
+              v-model="userInfo.gender"
+              :options="options"
+          ></b-form-radio-group>
         </b-col>
-     </b-row>
+      </b-row>
     </b-container>
 
     <!-- 사진 촬영 버튼 -->
-    
+
     <!-- 📌📌📌 작동 이상 없으면 삭제하기 !! -->
     <!-- <div class="container" v-if="!isCameraOpen">
       <div class="row d-flex justify-content-center m-md-2">
@@ -70,47 +76,51 @@
     <!-- 사진촬영 버튼 -->
     <div class="container">
       <div class="row d-flex justify-content-center">
-        
-          <button v-if="!isCameraOpen" 
-            class="btn-customm bg-red f-ujr" style="width: 60%"
-            :class="{
+
+        <button v-if="!isCameraOpen"
+                class="btn-customm bg-red f-ujr" style="width: 60%"
+                :class="{
               'bg-red': !isCameraOpen,
               'bg-green': isCameraOpen,
             }"
-            @click="checkInfo"
-          >
-            <span class="bg-red h4">사진 촬영</span>
-          </button>
+                @click="checkInfo"
+        >
+          <span class="bg-red h4">사진 촬영</span>
+        </button>
         <div class="camera-button"></div>
       </div>
       <div class="row d-flex justify-content-center m-md-2">
         <div class="camera-box" v-if="isCameraOpen">
           <video
-            v-show="!isPhotoTaken"
-            ref="camera"
-            id="Taken"
-            :width="300"
-            :height="300"
-            autoplay
+              v-show="!isPhotoTaken"
+              ref="camera"
+              id="Taken"
+              :width="300"
+              :height="300"
+              autoplay
           ></video>
           <canvas
-            v-show="isPhotoTaken"
-            ref="canvas"
-            id="photoTaken"
-            :width="300"
-            :height="300"
+              v-show="isPhotoTaken"
+              ref="canvas"
+              id="photoTaken"
+              :width="300"
+              :height="300"
           ></canvas>
         </div>
       </div>
 
-        <div class="camera-shoot mt-2 mb-5" v-if="isCameraOpen">
-          <button v-if="isPhotoTaken == false" class="btn-customm bg-red f-ujr h4" style="width: 60%" @click="takePhoto">사진촬영</button>
-          
-          <button v-if="isPhotoTaken == true" class="btn-customm bg-green f-ujr mr-4 h5" style="width: 30%" @click="takePhoto">다시찍기</button>
-          <router-link :to="{ name: 'FaceReadingResult', params: { userInfo: userInfo } }">
+      <div class="camera-shoot mt-2 mb-5" v-if="isCameraOpen">
+        <button v-if="isPhotoTaken == false" class="btn-customm bg-red f-ujr h4" style="width: 60%" @click="takePhoto">
+          사진촬영
+        </button>
+
+        <button v-if="isPhotoTaken == true" class="btn-customm bg-green f-ujr mr-4 h5" style="width: 30%"
+                @click="takePhoto">다시찍기
+        </button>
+        <router-link :to="{ name: 'FaceReadingResult', params: { userInfo: userInfo } }">
           <button v-if="isPhotoTaken == true" class="btn-customm f-ujr bg-red h5" style="width: 30%">관상보기</button>
-          </router-link>
-        </div>
+        </router-link>
+      </div>
 
       <!-- <div class="row d-flex justify-content-center m-md-2">
         <div class="camera-shoot"  v-if="isPhotoTaken">
@@ -123,6 +133,7 @@
 
 <script>
 import axios from "axios";
+
 const URL = "http://127.0.0.1:8000/services/face_reading/";
 export default {
   name: "FaceReading",
@@ -138,8 +149,8 @@ export default {
       },
       options: [
         // { value: null, text: "성별을 선택해주세요.", disabled: true },
-        { value: 1, text: "남자" },
-        { value: 2, text: "여자" },
+        {value: 1, text: "남자"},
+        {value: 2, text: "여자"},
       ],
       result: {
         eyebrowShape: null,
@@ -166,14 +177,11 @@ export default {
     checkInfo() {
       if (this.userInfo.nickname == "") {
         alert("닉네임을 입력해 주세요.");
-      }
-      else if (this.userInfo.age == null) {
+      } else if (this.userInfo.age == null) {
         alert("나이를 입력해 주세요.")
-      }
-      else if (this.userInfo.gender == null) {
+      } else if (this.userInfo.gender == null) {
         alert("성별을 선택해 주세요.")
-      }
-      else {
+      } else {
         this.toggleCamera()
       }
     },
@@ -199,14 +207,14 @@ export default {
       });
 
       navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then((stream) => {
-          this.$refs.camera.srcObject = stream;
-        })
-        .catch((err) => {
-          console.log(err);
-          alert("설정을 확인해주세요.");
-        });
+          .getUserMedia(constraints)
+          .then((stream) => {
+            this.$refs.camera.srcObject = stream;
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("설정을 확인해주세요.");
+          });
     },
     stopCameraStream() {
       let tracks = this.$refs.camera.srcObject.getTracks();
@@ -280,13 +288,13 @@ export default {
     },
     sendImage() {
       axios.post(`http://127.0.0.1:8000/services/test`, this.tmpphoto)
-      .then(res=> {
-        console.log(res)
-        console.log('보내짐')
-      })
-      .catch(err=> {
-        console.log(err)
-      })
+          .then(res => {
+            console.log(res)
+            console.log('보내짐')
+          })
+          .catch(err => {
+            console.log(err)
+          })
     },
   },
 };
@@ -303,21 +311,21 @@ export default {
 }
 
 .pt-20 {
-  padding-top:  20px;
+  padding-top: 20px;
 }
 
 .btn-customm {
-    display: inline-block;
-    font-weight: 400;
-    text-align: center;
-    vertical-align: middle;
-    background-color: (192, 0, 0);
-    user-select: none;
-    border: 1px solid transparent;
-    padding: 0.375rem 0.75rem;
-    font-size: 1rem;
-    line-height: 1.5;
-    border-radius: 0.25rem;
-    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  display: inline-block;
+  font-weight: 400;
+  text-align: center;
+  vertical-align: middle;
+  background-color: (192, 0, 0);
+  user-select: none;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 }
 </style>
