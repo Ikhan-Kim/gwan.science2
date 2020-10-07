5d7e6f416d01b77c17471a94d1e6e1ceacff3994
Young Hwan Kim <aorkslrdnjwm@naver.com>
2020-10-07 23:24:04 +0900

    [+] update job.py

--- Project/back/gwanscience/services/job.py
+++ Project/back/gwanscience/services/job.py
@@ -11,23 +11,23 @@ def job_classification(eyes, mouth, nose, glabella):
     nose_score = 0
     glabella_score = 0
 
-    if 0 <= eyes_size + eyes_tail + eyes_distance <=3:
+    if (2-eyes_size) + (2-eyes_tail) + (2-eyes_distance) >= 4:
         eyes_score = 8
     else:
         eyes_score = 0
 
     
-    if 0 <= lips_length + lips_thickness + mouth_tail <= 3:
+    if (2-lips_length) + lips_thickness + mouth_tail >= 3:
         mouth_score = 4
     else:
         mouth_score = 0
    
-    if 0 <= nose_length + nose_width <= 2:
+    if nose_length + (2-nose_width) >= 2:
         nose_score = 2
     else:
         nose_score = 0
 
-    if 0 <= glabella_distance < 1:
+    if 2 - glabella_distance >= 1:
         glabella_score = 1
     else:
         glabella_score = 0
