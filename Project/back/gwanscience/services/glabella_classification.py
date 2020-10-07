0d9767e9afc57fb3e580273bc2674198aeac10c3
김정원 <dnnls1@naver.com>
2020-10-07 17:35:04 +0900

    [+] update settings and classification

--- Project/back/gwanscience/services/glabella_classification.py
+++ Project/back/gwanscience/services/glabella_classification.py
@@ -11,25 +11,31 @@ from keras_preprocessing.image import ImageDataGenerator
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# target_image
-glabella_image_path = os.path.join(BASE_DIR, 'learning/classified/glabella_distance/test/big/283_eyebrow.jpg')
+def predict(filename):
+    # target_image
+    glabella_image_path = os.path.join(BASE_DIR, 'learning/classified/glabella_distance/test/big/283_eyebrow.jpg')
 
-# test target_image
-# glabella_image_path = os.path.join(BASE_DIR, 'learning/classified/glabella_distance/test/big/283_eyebrow.jpg')
+    # test target_image
+    # glabella_image_path = os.path.join(BASE_DIR, 'learning/classified/glabella_distance/test/big/283_eyebrow.jpg')
 
-image = tf.keras.preprocessing.image.load_img(
-    glabella_image_path,
-    grayscale=False,
-    color_mode='rgb',
-    target_size=(224,224), 
-)
+    image = tf.keras.preprocessing.image.load_img(
+        glabella_image_path,
+        grayscale=False,
+        color_mode='rgb',
+        target_size=(224,224), 
+    )
 
-image_arr = tf.keras.preprocessing.image.img_to_array(image)
-image = np.array([image_arr])/255.0
+    image_arr = tf.keras.preprocessing.image.img_to_array(image)
+    image = np.array([image_arr])/255.0
 
-# glabella_distance
-glabella_distance_model_path = os.path.join(BASE_DIR, 'learning/classified/training_glabella_distance_logs/model_glabella_distance4.h5')
-glabella_distance_model = tf.keras.models.load_model(glabella_distance_model_path)
+    # glabella_distance
+    glabella_distance_model_path = os.path.join(BASE_DIR, 'services/learning/classified/training_glabella_distance_logs/model_glabella_distance4.h5')
+    glabella_distance_model = tf.keras.models.load_model(glabella_distance_model_path)
 
-glabella_distance_classifications= glabella_distance_model.predict(image)
-print(np.argmax(glabella_distance_classifications[0])) # 0: big, 1: mid, 2: small,
+    glabella_distance_classifications= glabella_distance_model.predict(image)
+    # print(np.argmax(glabella_distance_classifications[0])) # 0: big, 1: mid, 2: small,
+    glabella_distance = np.argmax(glabella_distance_classifications[0])
+
+    data = [ glabella_distance ]
+
+    return data
--- Project/back/gwanscience/services/nose_classification.py
+++ Project/back/gwanscience/services/nose_classification.py
@@ -11,35 +11,42 @@ from keras_preprocessing.image import ImageDataGenerator
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# target_image
-nose_image_path = os.path.join(BASE_DIR, 'learning/classified/nose_length/test/mid/328_nose.jpg')
+def predict(filename):
+    # target_image
+    nose_image_path = os.path.join(BASE_DIR, 'learning/classified/nose_length/test/mid/328_nose.jpg')
 
-# test target_image
-# nose_image_path = os.path.join(BASE_DIR, 'learning/classified/nose_width/train/small/6_nose.jpg')
-# nose_image_path = os.path.join(BASE_DIR, 'learning/classified/nose_length/test/big/324_nose.jpg')
-# eyes_image_path = os.path.join(BASE_DIR, 'learning/classified/eyes_distance/test/big/336_nose.jpg')
+    # test target_image
+    # nose_image_path = os.path.join(BASE_DIR, 'learning/classified/nose_width/train/small/6_nose.jpg')
+    # nose_image_path = os.path.join(BASE_DIR, 'learning/classified/nose_length/test/big/324_nose.jpg')
+    # eyes_image_path = os.path.join(BASE_DIR, 'learning/classified/eyes_distance/test/big/336_nose.jpg')
 
-image = tf.keras.preprocessing.image.load_img(
-    nose_image_path,
-    grayscale=False,
-    color_mode='rgb',
-    target_size=(224,224), 
-)
+    image = tf.keras.preprocessing.image.load_img(
+        nose_image_path,
+        grayscale=False,
+        color_mode='rgb',
+        target_size=(224,224), 
+    )
 
-image_arr = tf.keras.preprocessing.image.img_to_array(image)
-image = np.array([image_arr])/255.0
+    image_arr = tf.keras.preprocessing.image.img_to_array(image)
+    image = np.array([image_arr])/255.0
 
-# nose_length
-nose_length_model_path = os.path.join(BASE_DIR, 'learning/classified/training_nose_length_logs/model_nose_length2.h5')
-nose_length_model = tf.keras.models.load_model(nose_length_model_path)
+    # nose_length
+    nose_length_model_path = os.path.join(BASE_DIR, 'services/learning/classified/training_nose_length_logs/model_nose_length2.h5')
+    nose_length_model = tf.keras.models.load_model(nose_length_model_path)
 
-nose_length_classifications = nose_length_model.predict(image)
-print(np.argmax(nose_length_classifications[0])) # 0: big, 1: mid, 2: small,
+    nose_length_classifications = nose_length_model.predict(image)
+    # print(np.argmax(nose_length_classifications[0])) # 0: big, 1: mid, 2: small,
+    nose_length = np.argmax(nose_length_classifications[0])
 
 
-# nose_width
-nose_width_model_path = os.path.join(BASE_DIR, 'learning/classified/training_nose_width_logs/model_nose_width2.h5')
-nose_width_model = tf.keras.models.load_model(nose_width_model_path)
+    # nose_width
+    nose_width_model_path = os.path.join(BASE_DIR, 'services/learning/classified/training_nose_width_logs/model_nose_width2.h5')
+    nose_width_model = tf.keras.models.load_model(nose_width_model_path)
 
-nose_width_classifications = nose_width_model.predict(image)
-print(np.argmax(nose_width_classifications[0])) # 0: big, 1: mid, 2: small,
+    nose_width_classifications = nose_width_model.predict(image)
+    # print(np.argmax(nose_width_classifications[0])) # 0: big, 1: mid, 2: small,
+    nose_width = np.argmax(nose_width_classifications[0])
+
+    data = [ nose_length, nose_width ]
+    
+    return data
